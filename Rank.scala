9843059af4f8243d574375e5ffd307ee4a3f5e86
Ji Yan <jiyan@uber.com>
2015-08-16 17:23:07 -0700

    Add code comment and clean up code format

--- Rank.scala
+++ Rank.scala
@@ -15,12 +15,16 @@ object Rank {
 	val edges = sqlContext.read.json("/251/graph/language_expertise/*")
 	val vertics = sqlContext.read.json("/251/graph/language_expertise_vertics/*")
 
+	// Construct follow graph
 	val verticsRdd: RDD[(Long, (String))] = vertics.rdd.map(v => (v(1).toString.toLong, (v(0).toString)))
-    val edgesRdd: RDD[Edge[String]] = edges.rdd.map(edge => Edge(edge(4).toString.toLong, edge(2).toString.toLong, edge(0).toString))
-    val graph = Graph(verticsRdd, edgesRdd)
+	val edgesRdd: RDD[Edge[String]] = edges.rdd.map(edge => Edge(edge(4).toString.toLong, edge(2).toString.toLong, edge(0).toString))
+	val graph = Graph(verticsRdd, edgesRdd)
 	
+	// Collect all possible programming languages in the graph
 	import sqlContext.implicits._
 	val languages = graph.edges.map(e => e.attr).distinct().collect()
+
+	// Run pagerank on a subset graph of each programming language
 	for (language <- languages) { val pagerankGraph = graph.subgraph(e => e.attr == language).pageRank(0.001); pagerankGraph.vertices.toDF.repartition(1).write.json("/251/graph/lang-pagerank/" + language)}
   }
 }
